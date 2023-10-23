392ea7cc78729b49f31112a421002b3add980d08
joardam <pedrofdias@live.com>
2023-10-23 18:30:57 -0300

    inicializando parte gráfica

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,45 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	int rows = 12;
+	int cols = 12;
+	int spriteSize = 32;
+
+	
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		int videoSizex = rows * spriteSize;
+        int videoSizey = cols * spriteSize;
+        
+        
+        Gdx.graphics.setWindowedMode(videoSizex,videoSizey);
+        Gdx.graphics.setResizable(false);
+        Gdx.graphics.setTitle("Campo Minado");
+        
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+        SpriteBatch spriteBatch = new SpriteBatch();
+        spriteBatch.begin();
+        spriteBatch.end();
+		
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		
 	}
 }
